d913b59e166c2dbcdfc312ce0484459e3feafd6c
Vitalik <05vp94@mail.ru>
2022-10-21 14:39:21 +0300

    Btn task start

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import './App.css';
-import Accordion from "./components/Accordion/Accordion";
-import Rating from "./components/Rating/Rating";
+import {BtnTask} from "./components/BtnTask/BtnTask";
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
 
-        <PageTitle title="First pages"/>
+      <BtnTask/>
 
-        <Rating value={2}/>
-        <Accordion title="Menu Title"/>
-        <Rating value={4}/>
-        <Rating value={1}/>
     </div>
   );
 }
 
 
-type PageTitlePropsType ={
-    title: string
-}
-function PageTitle(props: PageTitlePropsType) {
-    return <h1>{props.title}</h1>
-}
+
 
 export default App;
---
+++ src/components/BtnTask/BtnTask.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const BtnTask = () => {
+
+    return(
+        <div>
+            <button>1</button>
+            <button>2</button>
+        </div>
+    )
+}
